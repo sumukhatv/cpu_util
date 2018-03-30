9f21b00a629a7fb9b7cd349b6590ee1bd5fdd29b
Sumukha TV <sumukha_tumkur_vani@brown.edu>
2018-03-30 19:03:16 -0400

    script to get cpu usage value

---
+++ cpu_usage.py
@@ -0,0 +1,49 @@
+import time
+import string
+import sys
+import commands
+
+cpu_time_p = 0.0
+idle_time_p = 0.0
+
+def get_cpu_usage():
+	global cpu_time_p
+	global idle_time_p
+
+	text = commands.getoutput("cat /proc/stat | grep '^cpu '").split()
+	idle_time = float(text[4])
+	
+	cpu_time = float(text[1])+float(text[2])+float(text[3])+float(text[4])
+	
+	idle_d = idle_time - idle_time_p
+	cpu_time_d = cpu_time - cpu_time_p
+	usage_d = (1000*(cpu_time_d-idle_d)/cpu_time_d+5)/10
+	
+	cpu_time_p = cpu_time
+	idle_time_p = idle_time
+	
+	return usage_d
+
+def usage():
+	print "usage: python cpu_uage.py <interval seconds>"
+	return
+
+if __name__ == '__main__':
+	if (len(sys.argv) == 2):
+		if (sys.argv[1] == "-h"):
+			usage()
+			exit(0)
+
+		try:
+			while (True):
+				load = get_cpu_usage()
+				load = round(load, 2)
+				print load				
+				time.sleep(float(sys.argv[1]))
+
+		except KeyboardInterrupt:
+			exit(0)
+	
+	else:
+		usage()
+		
