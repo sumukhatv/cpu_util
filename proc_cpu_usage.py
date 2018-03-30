9dae2d6751bea71a473bd02c44fdb7c9fc677f7e
Sumukha TV <sumukha_tumkur_vani@brown.edu>
2018-03-29 22:51:50 -0400

    script to get CPU usage for a particular process

---
+++ proc_cpu_usage.py
@@ -0,0 +1,36 @@
+import time
+import string
+import sys
+import commands
+
+def get_cpu_usage(proc):
+	for entry in commands.getoutput("ps aux").split("\n"):
+		if (proc in entry.split()[10]):
+			return float(entry.split()[2])
+	return 0.0
+
+def usage():
+	print "usage: python proc_cpu_uage.py <interval seconds> <process name> <output filename>"
+	return
+
+if __name__ == '__main__':
+	if (len(sys.argv) == 2):
+		if (sys.argv[1] == "-h"):
+			usage()
+			exit(0)
+	
+	if (len(sys.argv) == 4):
+		f = open(sys.argv[3], "w")
+		print("%CPU")
+    		f.write("%CPU\n")
+    		try:
+        		while True:
+            			load = get_cpu_usage(sys.argv[2])
+            			if load:
+            				print("%.2f" % (load))
+					f.write("%.2f\n" % (load))
+            			time.sleep(float(sys.argv[1]))
+    		except KeyboardInterrupt:
+        		exit(0)
+	else:
+		usage()
